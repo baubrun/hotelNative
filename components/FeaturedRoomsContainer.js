621b37bcaabc65d63df76d3852f3c376ca6bd392
baubrun <baubelf@gmail.com>
2021-08-10 14:55:22 -0400

    added api

---
+++ components/FeaturedRoomsContainer.js
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, FlatList, View, Image} from 'react-native';
+
+const FeaturedRoomsContainer = () => {
+  useEffect(() => {}, []);
+
+  const [featuredRooms, setFeaturedRooms] = useState([]);
+  return <View></View>;
+};
+
+export default FeaturedRoomsContainer;
